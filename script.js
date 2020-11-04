0c500729ac696b6fd08eb97f3babb4065633d5d8
AWSiegfried <awsiegfried@gmail.com>
2020-11-04 08:10:25 -0800

    updated js to link separate code blocks

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ var quiz = document.querySelector("#main");
 var orderedList = document.querySelector("#orderedList");
 var welcome = document.querySelector("#welcome");
 var timerSent = document.querySelector("#timer");
-var highscore = document.querySelector("#highscore");
+var currentScore = document.querySelector("#currentScore");
 var button = document.querySelector("#button");
 var grade = document.querySelector("#grade");
-var answer1 = document.querySelector("#answer1")
-var answer2 = document.querySelector("#answer2")
-var answer3 = document.querySelector("#answer3")
-var answer4 = document.querySelector("#answer4")
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+var finalScore = document.querySelector("#final-score");
+var answers = document.querySelectorAll(".answerClass")
+var currentQuestion = document.querySelector("#theQuestion");
+var gameover = document.querySelector("#gameover")
+
 
 
 //Gamestate variables
@@ -17,41 +22,24 @@ var thisQuestion = 0;
 var timer = 60;
 var score = 0;
 
-
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    //Hide welcome message
-    welcome.style.display = "none";
-    //Begin quiz
-    askQuestions();
-});
-
 //Array of questions
 var questions = [{
     thequestion: "Do you like pigs?",
-    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Goddamn that guy"],
-    answer: "Goddamn that guy",
+    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Hate that guy"],
+    answer: "3",
 }, {
     thequestion: "Do you like anime?",
-    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Goddamn that guy"],
-    answer: "Goddamn that guy",
+    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Hate that guy"],
+    answer: "3",
 }, {
     thequestion: "Are you the reincarnated ghost of Ghengis Khan?",
-    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Goddamn that guy"],
-    answer: "Goddamn that guy",
+    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Hate that guy"],
+    answer: "3",
 }, {
     thequestion: "What does Pot of Greed do?",
-    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Goddamn that guy"],
-    answer: "Goddamn that guy",
+    options: ["Heck yes", "Goopen No", "Big friggin yessir", "Hate that guy"],
+    answer: "3",
 }];
-
-
-
-//User clicks intended answer
-//Declare right or wrong (matches object property yes or no)
-//Change time
-//Next question
 
 //For loop cycling through each question.
 function askQuestions() {
@@ -62,54 +50,89 @@ function askQuestions() {
             timerSent.textContent = timer + " seconds left";
             timer--;
 
-            if (timer === 0) {
+            if (timer <= 0) {
                 clearInterval(timerInterval);
                 //End Game (state Game Over, go to results page)
-                return ""
+                sendMessage();
             }
         }, 1000);
     }
 
+    function sendMessage() {
+        welcome.style.display = "none";
+        gameover.style.display = "block";
+        finalScore.textContent = score;
+        console.log(score)
+    }
+
 
     startTimer();
-
-    //Present questions
-    var currentQuestion = document.querySelector("#theQuestion");
-    currentQuestion.textContent = questions[thisQuestion].thequestion;
-
-
-    //Present 4 potential answers
-    answer1.textContent = questions[thisQuestion].options[0]
-    answer2.textContent = questions[thisQuestion].options[1]
-    answer3.textContent = questions[thisQuestion].options[2]
-    answer4.textContent = questions[thisQuestion].options[3]
+    rerender();
 
     //Make them clickable
     function click(event) {
-        var target = event.target.textContent;
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(this);
+        var answerIndex = this.dataset.index;
         //Declare right or wrong by comparing answer in question object to clicked choice
-        if (target === questions[thisQuestion].answer) {
+        if (answerIndex === questions[thisQuestion].answer) {
             //Going to have to add time here
             grade.textContent = "Zip Zap Correct Is That"
             score += 10;
             timer += 5;
-            //Clear old question 
-            orderedList.innerHTML = "";
-            currentQuestion.textContent = "";
         } else {
             grade.textContent = "Boom Bammed Shoulda Crammed"
             timer -= 10;
-            //Clear old question 
-            orderedList.innerHTML = "";
-            currentQuestion.textContent = "";
+        }
+        //Clear old question 
+        answers.forEach(function(answer) {
+            answer.textContent = "";
+        })
+        currentQuestion.textContent = "";
+        setTimeout(function() {
+            grade.textContent = "";
+            thisQuestion++;
+            if (thisQuestion === questions.length) {
+                sendMessage();
+            } else {
+                rerender();
+            }
+        }, 1000);
+        // clearTimeout(timeId)
+
+    }
+
+    function rerender() {
+        console.log(thisQuestion)
+            //Present Score
+        currentScore.textContent = "Score: " + score;
+
+        //Present questions
+        currentQuestion.textContent = questions[thisQuestion].thequestion;
+
+
+        //Present 4 potential answers
+
+        for (let index = 0; index < answers.length; index++) {
+            var element = answers[index];
+            element.textContent = questions[thisQuestion].options[index]
+        }
+
+        for (let index = 0; index < answers.length; index++) {
+            var element = answers[index];
+            element.addEventListener("click", click);
         }
     }
-    document.addEventListener("click", click);
-    //Go to next question
-    thisQuestion++;
-
-    //Go to the next question
+    //Highscore Page
 }
-// Add timer
-// Add score
-// Highscore page
+
+//Start Game
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    //Hide welcome message
+    welcome.style.display = "none";
+    //Begin quiz
+    askQuestions();
+});
